9a787c18dd3596929841f2eb56eda48d2b1f7d87
Waysyy <74637067+Waysyy@users.noreply.github.com>
2021-11-27 15:07:01 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace integrall
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.verh = new System.Windows.Forms.TextBox();
+            this.niz = new System.Windows.Forms.TextBox();
+            this.epsil = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.zedGraph = new ZedGraph.ZedGraphControl();
+            this.function = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.прямоугльникToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.трапецияToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.симпсонToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.построитьГрафикToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // verh
+            // 
+            resources.ApplyResources(this.verh, "verh");
+            this.verh.Name = "verh";
+            this.verh.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.verh_KeyPress);
+            // 
+            // niz
+            // 
+            resources.ApplyResources(this.niz, "niz");
+            this.niz.Name = "niz";
+            this.niz.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.verh_KeyPress);
+            // 
+            // epsil
+            // 
+            resources.ApplyResources(this.epsil, "epsil");
+            this.epsil.Name = "epsil";
+            this.epsil.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.verh_KeyPress);
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            // 
+            // zedGraph
+            // 
+            resources.ApplyResources(this.zedGraph, "zedGraph");
+            this.zedGraph.Name = "zedGraph";
+            this.zedGraph.ScrollGrace = 0D;
+            this.zedGraph.ScrollMaxX = 0D;
+            this.zedGraph.ScrollMaxY = 0D;
+            this.zedGraph.ScrollMaxY2 = 0D;
+            this.zedGraph.ScrollMinX = 0D;
+            this.zedGraph.ScrollMinY = 0D;
+            this.zedGraph.ScrollMinY2 = 0D;
+            // 
+            // function
+            // 
+            resources.ApplyResources(this.function, "function");
+            this.function.Name = "function";
+            // 
+            // label5
+            // 
+            resources.ApplyResources(this.label5, "label5");
+            this.label5.Name = "label5";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.прямоугльникToolStripMenuItem,
+            this.трапецияToolStripMenuItem,
+            this.симпсонToolStripMenuItem,
+            this.построитьГрафикToolStripMenuItem});
+            resources.ApplyResources(this.menuStrip1, "menuStrip1");
+            this.menuStrip1.Name = "menuStrip1";
+            // 
+            // прямоугльникToolStripMenuItem
+            // 
+            this.прямоугльникToolStripMenuItem.Name = "прямоугльникToolStripMenuItem";
+            resources.ApplyResources(this.прямоугльникToolStripMenuItem, "прямоугльникToolStripMenuItem");
+            this.прямоугльникToolStripMenuItem.Click += new System.EventHandler(this.прямоугльникToolStripMenuItem_Click);
+            // 
+            // трапецияToolStripMenuItem
+            // 
+            this.трапецияToolStripMenuItem.Name = "трапецияToolStripMenuItem";
+            resources.ApplyResources(this.трапецияToolStripMenuItem, "трапецияToolStripMenuItem");
+            this.трапецияToolStripMenuItem.Click += new System.EventHandler(this.трапецияToolStripMenuItem_Click);
+            // 
+            // симпсонToolStripMenuItem
+            // 
+            this.симпсонToolStripMenuItem.Name = "симпсонToolStripMenuItem";
+            resources.ApplyResources(this.симпсонToolStripMenuItem, "симпсонToolStripMenuItem");
+            this.симпсонToolStripMenuItem.Click += new System.EventHandler(this.симпсонToolStripMenuItem_Click);
+            // 
+            // построитьГрафикToolStripMenuItem
+            // 
+            this.построитьГрафикToolStripMenuItem.Name = "построитьГрафикToolStripMenuItem";
+            resources.ApplyResources(this.построитьГрафикToolStripMenuItem, "построитьГрафикToolStripMenuItem");
+            this.построитьГрафикToolStripMenuItem.Click += new System.EventHandler(this.построитьГрафикToolStripMenuItem_Click);
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            resources.ApplyResources(this.imageList1, "imageList1");
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // pictureBox1
+            // 
+            resources.ApplyResources(this.pictureBox1, "pictureBox1");
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.function);
+            this.Controls.Add(this.zedGraph);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.epsil);
+            this.Controls.Add(this.niz);
+            this.Controls.Add(this.verh);
+            this.HelpButton = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox verh;
+        private System.Windows.Forms.TextBox niz;
+        private System.Windows.Forms.TextBox epsil;
+        private System.Windows.Forms.Label label4;
+        private ZedGraph.ZedGraphControl zedGraph;
+        private System.Windows.Forms.TextBox function;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem прямоугльникToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem трапецияToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem симпсонToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem построитьГрафикToolStripMenuItem;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,319 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using ZedGraph;
+using MathNet.Symbolics;
+using Express = MathNet.Symbolics.SymbolicExpression;
+
+
+namespace integrall
+{
+    public partial class Form1 : Form
+    {
+        private delegate void UpdateLabelCallback(double S);
+        private delegate void UpdatePanelCallback(int[] array);
+        public Form1()
+        {
+            InitializeComponent();
+            DrawGraph();
+        }
+        double s;
+
+        private void verh_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!(Char.IsDigit(e.KeyChar)))
+            {
+                if (e.KeyChar != (char)Keys.Back != (e.KeyChar == ',') != (e.KeyChar == '-'))
+                {
+                    e.Handled = true;
+                }
+            }
+        }
+        
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            //result.ReadOnly = true;
+            
+        }
+
+        
+            
+        
+        private void DrawGraph()
+        {
+            GraphPane pane = zedGraph.GraphPane;
+            pane.CurveList.Clear();
+            pane.YAxis.Title.Text = "Ось Y";
+            pane.XAxis.Title.Text = "Ось X";
+            pane.Title.Text = "Построение графика функции";
+            pane.XAxis.Cross = 0.0;
+            pane.YAxis.Cross = 0.0;
+            pane.XAxis.Scale.IsSkipFirstLabel = true;
+            pane.XAxis.Scale.IsSkipLastLabel = true;
+            pane.XAxis.Scale.IsSkipCrossLabel = true;
+            pane.YAxis.Scale.IsSkipFirstLabel = true;
+            pane.YAxis.Scale.IsSkipLastLabel = true;
+            pane.YAxis.Scale.IsSkipCrossLabel = true;
+            pane.IsFontsScaled = false;
+            pane.IsBoundedRanges = true;
+            
+
+            zedGraph.AxisChange();
+
+        }
+
+        private void прямоугльникToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Threading.Thread readThread = new System.Threading.Thread(new System.Threading.ThreadStart(pr));
+            readThread.Start();
+        }
+
+        public void pr()
+        {
+            try
+            {
+                long ellapledTicks = DateTime.Now.Ticks;
+                var expr = Express.Parse(function.Text);
+                Func<double, double> f = expr.Compile("x");
+
+                double a = Convert.ToDouble(niz.Text);
+                double b = Convert.ToDouble(verh.Text);
+                double eps = Convert.ToDouble(epsil.Text);
+                if (b > a && eps > 0)
+                {
+
+                    int n = 10;
+                    double sum, sum2 = 0;
+                    do
+                    {
+                        sum = sum2;
+                        sum2 = 0;
+                        double h = (b - a) / n;
+                        double x = a + h;
+
+                        while (x <= b)
+                        {
+                            sum2 += (f(x) + f(x + h)) * h / 2;
+                            x += h;
+                        }
+
+
+
+                        n *= 2;
+                    } while (Math.Abs(sum - sum2) > eps);
+                    double S = sum2;
+                    label3.Invoke(new UpdateLabelCallback(UpdateLabel3), S);
+                    s = n;
+
+                }
+                else
+                {
+                    MessageBox.Show("Проверьте правильность ввода данных!", "Ошибка!");
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Проверьте правильность ввода данных!", "Ошибка!");
+            }
+        }
+
+        private void трапецияToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Threading.Thread readThread = new System.Threading.Thread(new System.Threading.ThreadStart(tr));
+            readThread.Start();
+
+        }
+
+        public void tr()
+        {
+            try
+            {
+
+                var expr = Express.Parse(function.Text);
+                Func<double, double> f = expr.Compile("x");
+
+                double a = Convert.ToDouble(niz.Text);
+                double b = Convert.ToDouble(verh.Text);
+                double eps = Convert.ToDouble(epsil.Text);
+                if (b > a && eps > 0)
+                {
+
+
+                    int n = 10;
+                    double sum, sum2 = 0;
+                    do
+                    {
+                        sum = sum2;
+                        sum2 = 0;
+                        double h = (b - a) / n;
+                        double x = a + h;
+
+
+                        while (x <= b)
+                        {
+                            sum2 += (f(x) + f(x + h)) * h / 2;
+                            x += h;
+                        }
+
+
+                        n *= 2;
+                    } while (Math.Abs(sum - sum2) > eps);
+                    double S = sum2;
+                    label3.Invoke(new UpdateLabelCallback(UpdateLabel3), S);
+                    s = n;
+                }
+                else
+                {
+                    MessageBox.Show("Проверьте правильность ввода данных!", "Ошибка!");
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Проверьте правильность ввода данных!", "Ошибка!");
+            }
+        }
+
+        private void симпсонToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Threading.Thread readThread = new System.Threading.Thread(new System.Threading.ThreadStart(simpson));
+            readThread.Start();
+
+        }
+
+        public void simpson ()
+        {
+            try
+            {
+
+                var expr = Express.Parse(function.Text);
+                Func<double, double> f = expr.Compile("x");
+
+                double a = Convert.ToDouble(niz.Text);
+                double b = Convert.ToDouble(verh.Text);
+                double eps = Convert.ToDouble(epsil.Text);
+                if (b > a && eps > 0)
+                {
+
+                    int n = 10;
+
+                    var h = (b - a) / n;
+
+                    var sum1 = 0d;
+                    var sum2 = 0d;
+
+                    for (var k = 1; k <= n; k++)
+                    {
+                        var xk = a + k * h;
+                        if (k <= n - 1)
+                        {
+                            sum1 += f(xk);
+                        }
+
+                        var xk_1 = a + (k - 1) * h;
+                        sum2 += f((xk + xk_1) / 2);
+                    }
+
+                    double S = h / 3d * (1d / 2d * f(a) + sum1 + 2 * sum2 + 1d / 2d * f(b));
+                    n *= 2;
+
+                    label3.Invoke(new UpdateLabelCallback(UpdateLabel3), S);
+                    //result.Text = "Ответ: " + resul.ToString();
+                    s = n;
+                }
+                else
+                {
+                    MessageBox.Show("Проверьте правильность ввода данных!", "Ошибка!");
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Проверьте правильность ввода данных!", "Ошибка!");
+            }
+        }
+
+        private void UpdateLabel3(double S)
+        {
+            label3.Text = Convert.ToString(S);
+
+        }
+        private void построитьГрафикToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Threading.Thread readThread = new System.Threading.Thread(new System.Threading.ThreadStart(UpdatePanel));
+            readThread.Start();
+        }
+
+        private void UpdatePanel()
+        {
+            try
+            {
+                if (s != 0)
+                {
+                    var expr = Express.Parse(function.Text);
+                    Func<double, double> f = expr.Compile("x");
+                    GraphPane pane = zedGraph.GraphPane;
+                    pane.CurveList.Clear();
+                    PointPairList list = new PointPairList();
+                    LineItem myCurve = pane.AddCurve("Sinc", list, Color.Red, SymbolType.None);
+
+                    double a = Convert.ToDouble(niz.Text);
+                    double b = Convert.ToDouble(verh.Text);
+                    double h = (b - a) / s;
+
+                    for (double x = a; x <= b; x += h)
+                    {
+                        list.Add(x, f(x));
+                    }
+
+                    int lenght = 1;
+
+                    for (double x = a; x <= b; x += h)
+                    {
+                        ++lenght;
+                    }
+
+                    // Высота столбиков
+                    double[] values = new double[lenght];
+
+                    double[] val2 = new double[lenght];
+                    int i = 0;
+                    for (double x = a; x <= b; x += h, ++i)
+                    {
+                        values[i] = f(x);
+                        val2[i] = x;
+
+                        System.Threading.Thread.Sleep(1000);
+                        BarItem curve = pane.AddBar("", val2, values, Color.Pink);
+
+                        zedGraph.AxisChange();
+                        zedGraph.Invalidate();
+                    }
+
+                    /*double[] val2 = new double[lenght];
+
+                    i = 0;
+                    for (double x = a; x <= b; x += h, ++i)
+                    {
+                        val2[i] = x;
+                        
+                    }*/
+
+                    
+                }
+                else
+                {
+                    MessageBox.Show("Необходимо решить интеграл", "Ошибка!");
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Проверьте правильность ввода данных!", "Ошибка!");
+            }
+
+        }
+    }
+}
+
+
